6e2b21cc9c9e07bb5018da7b77ebd8ae4355f237
John the Scott <jmscott@setspace.com>
2018-02-23 21:50:24 -0600

    logger_test: add .svn-ignore

--- golang/logger/logger_test.go
+++ golang/logger/logger_test.go
@@ -7,8 +7,13 @@ import (
 
 const heartbeat_pause = 2 * time.Second
 
-func test_open(what string, t *testing.T) (*Logger, bool) {
-	log, err := Open("test", "Dow", HeartbeatPause(heartbeat_pause))
+func test_open(
+	what string,
+	t *testing.T,
+	options ...option,
+) (*Logger, bool) {
+
+	log, err := Open("test", "Dow", options...)
 	if err != nil {
 		t.Fatalf("Open(%s) failed: %s", what, err)
 		return nil, false
@@ -21,12 +26,18 @@ func test_open(what string, t *testing.T) (*Logger, bool) {
 	log.ERROR("%s: hi", what)
 	return log, true
 }
+
 func TestHeartbeat(t *testing.T) {
 
-	log, ok := test_open("TestHeartbeat", t)
+	log, ok := test_open(
+			"TestHeartbeat",
+			t,
+			HeartbeatPause(heartbeat_pause),
+	)
 	if !ok {
 		return
 	}
+	defer log.Close()
 
 	//  test heartbeat
 
@@ -34,6 +45,17 @@ func TestHeartbeat(t *testing.T) {
 	log.INFO("sleep %s", sleep_pause)
 	time.Sleep(sleep_pause)
 	log.INFO("awoke from %s sleep", sleep_pause)
+}
 
+func TestDirectory(t *testing.T) {
+	log, ok := test_open(
+			"TestDirectory",
+			t,
+			HeartbeatPause(heartbeat_pause),
+			Directory("tmp"),
+	)
+	if !ok {
+		return
+	}
 	defer log.Close()
 }
