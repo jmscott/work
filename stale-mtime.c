a55298aaeca532ee4d69606cec526d128f125869
John the Scott <jmscott@setspace.com>
2022-01-01 22:41:16 -0600

    refactored for new include/jmscott

--- stale-mtime.c
+++ stale-mtime.c
@@ -8,8 +8,7 @@
  *	0	file modify time is greater than or equal to now - <seconds>.
  *	1	file modify time is less than now - <seconds>.
  *	2	file does not exist.
- *	3	command line invocation error.
- *	4	bad stat() system call.
+ *	3	unexpected error
  */
 #include <sys/errno.h>
 #include <sys/stat.h>
@@ -19,39 +18,27 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#include "jmscott/die.c"
+
 #define EXIT_STALE	0
 #define EXIT_FRESH	1
 #define EXIT_NO_EXIST	2
-#define EXIT_BAD_INVO	3
-#define EXIT_BAD_STAT	4
+#define EXIT_FAULT	3
 
 extern int	errno;
 
-static char *prog = "stale-mtime";
+char *jmscott_progname = "stale-mtime";
 
 static void
 die(char *msg, int exit_status)
 {
-	char buf[1024];
-
-	strcpy(buf, prog);
-	strcat(buf, ": ERROR: ");
-	strcat(buf, msg);
-	strcat(buf, "\n");
-
-	write(2, buf, strlen(buf));
-	exit(exit_status);
+	jmscott_die(exit_status, msg);
 }
 
 static void
 die2(char *msg1, char *msg2, int exit_status)
 {
-	char buf[1024];
-
-	strcpy(buf, msg1);
-	strcat(buf, ": ");
-	strcat(buf, msg2);
-	die(buf, exit_status);
+	jmscott_die2(exit_status, msg1, msg2);
 }
 
 int
@@ -62,7 +49,7 @@ main(int argc, char **argv)
 	time(&now);
 
 	if (argc != 3)
-		die("wrong number of arguments", EXIT_BAD_INVO);
+		die("wrong number of arguments", EXIT_FAULT);
 
 	/*
 	 *  Verify the number of seconds of staleness is >= 0 and < 999999999.
@@ -70,18 +57,18 @@ main(int argc, char **argv)
 
 	char *sec = argv[1];
 	if (strlen(sec) > 9)
-		die("too many chars in seconds: expected <= 9", EXIT_BAD_INVO);
+		die("too many chars in seconds: expected <= 9", EXIT_FAULT);
 	for (char *p = sec;  *p;  p++) {
 		char c = *p;
 
 		if (c < '0' || c > '9')
-			die("non digit character in seconds", EXIT_BAD_INVO);
+			die("non digit character in seconds", EXIT_FAULT);
 	}
 	int stale_after = now - (time_t)atoi(sec);
 
 	char *path = argv[2]; 
 	if (strlen(path) >= 256)
-		die("too many characters in file path (>= 256)", EXIT_BAD_INVO);
+		die("too many characters in file path (>= 256)", EXIT_FAULT);
 
 	//  stat() the file to determine freshness.
 	struct stat st;
@@ -89,7 +76,7 @@ main(int argc, char **argv)
 	if (stat(path, &st) < 0) {
 		if (errno == ENOENT)
 			exit(EXIT_NO_EXIST);
-		die2(strerror(errno), path, EXIT_BAD_STAT);
+		die2(strerror(errno), path, EXIT_FAULT);
 	}
 	if (st.st_mtime < stale_after)
 		exit(EXIT_STALE);
