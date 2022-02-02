a06ec608954fbbb12941cd55a7ae3bfc7b06743f
John the Scott <jmscott@setspace.com>
2022-02-01 23:08:09 -0600

    added tracing to jmscott_json_write()

--- clang/json.c
+++ clang/json.c
@@ -11,8 +11,17 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 #include "jmscott/posio.c"
+
+struct jmscott_json
+{
+	int		out_fd;
+	int		indent;
+
+	int		trace;
+};
 
 /*
  *  Convert an ascii string to escaped json "string".
@@ -78,6 +87,43 @@ jmscott_ascii2json_string(char *src, char *tgt, int tgt_size)
 	return (char *)0;
 }
 
+struct jmscott_json *
+jmscott_json_new()
+{
+	struct jmscott_json *jp = malloc(sizeof (struct jmscott_json));
+	if (!jp)
+		return (struct jmscott_json *)0;
+	jp->out_fd = 1;
+	jp->indent = 0;
+	jp->trace = 0;
+	return jp;
+}
+
+void
+jmscott_json_trace(struct jmscott_json *jp, char *what, char *value)
+{
+	if (!jp->trace)
+		return;
+	write(2, "TRACE: ", 7);
+	write(2, what, strlen(what));
+	write(2, ": ", 2);
+	write(2, value, strlen(value));
+	write(2, "\n", 1);
+}
+
+void
+jmscott_json_trace_c(struct jmscott_json *jp, char c)
+{
+	if (c) {
+		char buf[2];
+
+		buf[0] = c;
+		buf[1] = 0;
+		jmscott_json_trace(jp, "c", buf);
+	} else
+		jmscott_json_trace(jp, "c", "null");
+}
+
 /*
  *  Synopsis:
  *	Write json structures described in a format string.
@@ -86,39 +132,47 @@ jmscott_ascii2json_string(char *src, char *tgt, int tgt_size)
  *	variable arguments list.
  *
  *		jmscott_json_write("s", "hello, world");
+ *
+ *	Important to note that correctness of json syntax is NOT insured!
  */
 char *
-jmscott_json_write(int fd, char *format, ...)
+jmscott_json_write(struct jmscott_json *jp, char *format, ...)
 {
+	jmscott_json_trace(jp, "format", format);
+/*
+ *  Note: poor man's closure
+ */
 #define RETURN(e) {va_end(argv);  return(e);}
 
 #define WRITE(s)							\
-	{if (jmscott_write(1, s, strlen(s)))				\
-		RETURN(strerror(errno));}
-#define INDENT(level)							\
-	for (int i = 0;  i < level;  i++)				\
-		WRITE("\t");
+	{								\
+		if (jmscott_write(jp->out_fd, s, strlen(s)))		\
+			RETURN(strerror(errno));			\
+	}
+
+#define INDENT								\
+	{								\
+		for (int i = 0;  i < jp->indent;  i++)			\
+			WRITE("\t");					\
+	}
 
 	char *f, c;
 	va_list argv;
-	(void)fd;
 	char *err;
-	int indent = 1;
 
 	va_start(argv, format);
 
 	f = format;
 	if (!f)
 		RETURN("null format");
-
 step:
 	//  skip white space in format command
 	while ((c = *f++) && isspace(c))
 		;
+	jmscott_json_trace_c(jp, c);
 	if (!c)
 		RETURN((char *)0);
 	switch (c) {
-
 	//  # comment
 	case '#':
 		while ((c = *f++) && c != '\n')
@@ -126,19 +180,45 @@ step:
 		if (!c)
 			RETURN((char *)0);
 		break;
+	case 'k':
+		INDENT;
+		//  fallthrough to case 's':
 	
 	//  json string
 	case 's': {
 		char json[1024 * 64];
 
 		char *a = va_arg(argv, char *);
+
+		jmscott_json_trace(jp, "s", a);
 		if ((err = jmscott_ascii2json_string(a, json, sizeof json)))
 			RETURN(err);
+		jmscott_json_trace(jp, "s+", json);
 		WRITE(json);
 		break;
+	case '{': 
+		INDENT;
+		WRITE("{\n");
+		jp->indent++;
+		break;
+	case '[':
+		INDENT;
+		WRITE("[\n");
+		jp->indent++;
+		break;
+	case '}':
+		--jp->indent;
+		INDENT;
+		WRITE("}\n");
+		break;
 	}
+	case ']':
+		--jp->indent;
+		INDENT;
+		WRITE("]\n");
+		break;
 	default:
-		if (jmscott_write(fd, &c, 1))
+		if (jmscott_write(jp->out_fd, &c, 1))
 			RETURN(strerror(errno));
 	}
 	goto step;
