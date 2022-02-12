3bdd8065f8b886b4d80a10e763094c1459982202
John the Scott <jmscott@setspace.com>
2022-02-12 16:09:26 -0600

    added jmscott_RFC3339_timeval(), zapped apple time fluff

--- clang/time.c
+++ clang/time.c
@@ -5,26 +5,41 @@
 #ifndef JMSCOTT_CLANG_TIME
 #define JMSCOTT_CLANG_TIME
 
+#include <sys/time.h>
 #include <time.h>
 #include <sys/errno.h>
 #include <string.h>
-
 #include <stdio.h>
 
-#if __APPLE__ == 1 && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 101200
-
-typedef enum {
-	CLOCK_REALTIME,
-	CLOCK_MONOTONIC,
-	CLOCK_PROCESS_CPUTIME_ID,
-	CLOCK_THREAD_CPUTIME_ID
-} clockid_t;
-
-extern int clock_gettime(clockid_t, struct timespec *);
-
-#endif	/* APPLE MAC OSX */
-
 static char	*RFC3339Nano = "%04d-%02d-%02dT%02d:%02d:%02d.%09ld+00:00";
+static char	*RFC3339Micro = "%04d-%02d-%02dT%02d:%02d:%02d.%03ld+00:00";
+
+/*
+ *  Note:
+ *	Will "struct timeval" become obsolete?
+ */
+char *
+jmscott_RFC3339_timeval(char *buf, int buf_size, struct timeval *tv)
+{
+	if (!tv)
+		return "null timeval";
+	struct tm *t = gmtime(&tv->tv_sec);
+	if (!t)
+		return strerror(errno);
+
+	/*
+	 *  Format the buffer.
+	 */
+	return snprintf(buf, buf_size, RFC3339Micro,
+		t->tm_year + 1900,
+		t->tm_mon + 1,
+		t->tm_mday,
+		t->tm_hour,
+		t->tm_min,
+		t->tm_sec,
+		tv->tv_usec
+	) < buf_size ? (char *)0 : "time buffer too small";
+}
 
 char *
 jmscott_RFC3339Nano_now(char *buf, int buf_size)
@@ -38,7 +53,7 @@ jmscott_RFC3339Nano_now(char *buf, int buf_size)
 	if (t == NULL)
 		return strerror(errno);
 	/*
-	 *  Format the record buffer.
+	 *  Format the time buffer.
 	 */
 	return snprintf(buf, buf_size, RFC3339Nano,
 		t->tm_year + 1900,
