2c78d1e33e9e9e2681338e0c0e3e08b39c2b670b
John the Scott <jmscott@setspace.com>
2023-03-28 15:33:39 -0500

    apple stable version

---
+++ clib/sendfile.c
@@ -0,0 +1,64 @@
+/*
+ *  Synopsis:
+ *	Portable interface to non-posix sendfile().
+ */
+#include <sys/errno.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "jmscott/libjmscott.h"
+
+extern int	errno;
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+
+//  mac os (<ventura) can not sendfile on non socket, so brute force
+
+static char *
+send_not_socket(int in, int out, off_t offset, off_t len)
+{
+	int status = jmscott_lseek(in, offset, SEEK_SET);
+	if (status < 0)
+		return strerror(status);
+
+	int nr, nw;
+	char buf[4096];
+	while ((nr = jmscott_read(in, buf, sizeof buf)) > 0) {
+		if ((nw = jmscott_write_all(out, buf, nr)) < 0)
+			return strerror(errno);
+		len -= nw;
+		if (len == 0)
+			return (char *)0;
+	}
+	if (nr < 0)
+		return strerror(errno);
+	if (len > 0)
+		return "not all bytes written";
+	return (char *)0;
+}
+
+char *
+jmscott_sendfile(int in, int out, off_t offset, off_t len)
+{
+	int total = len;
+	int status;
+AGAIN:
+	status = sendfile(in, out, offset, &len, (struct sf_hdtr *)0, 0);
+	if (status != 0) {
+		if (errno == ENOTSOCK)
+			return send_not_socket(in, out, offset, len);
+		if (errno == EAGAIN)
+			goto AGAIN;
+		return strerror(errno);
+	}
+	if (len == 0)
+		return "unexpected end of file";
+	total -= len;
+	if (total == 0)
+		return (char *)0;
+	offset += len;
+	len = total;
+	goto AGAIN;
+}
