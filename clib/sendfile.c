9fb71641bf29e8a3d42251bb9f03fd873a148bf4
John the Scott <jmscott@setspace.com>
2023-08-19 03:35:39 -0500

    second rewrite for apple brained sendfile

--- clib/sendfile.c
+++ clib/sendfile.c
@@ -1,6 +1,8 @@
 /*
  *  Synopsis:
  *	Portable interface to non-posix sendfile().
+ *  Note:
+ *	Move sendfile.c to file.c, so other helper file funcs can be added.
  */
 #include <sys/errno.h>
 #include <string.h>
@@ -18,56 +20,91 @@ extern int	errno;
 #include <sys/sendfile.h>
 #endif
 
-//  mac os (<ventura) can not sendfile on non socket, so brute force
-
 static char *
-send_not_socket(int in, int out, off_t offset, off_t len)
+copyio(int in, int out, long long *send_size)
 {
-	int status = jmscott_lseek(in, offset, SEEK_SET);
-	if (status < 0)
-		return strerror(status);
+	int nr, nw, total = 0;
 
-	int nr, nw;
 	char buf[4096];
 	while ((nr = jmscott_read(in, buf, sizeof buf)) > 0) {
 		if ((nw = jmscott_write_all(out, buf, nr)) < 0)
 			return strerror(errno);
-		len -= nw;
-		if (len == 0)
+		total += nw;
+		if (total == *send_size)
 			return (char *)0;
 	}
 	if (nr < 0)
 		return strerror(errno);
-	if (len > 0)
-		return "not all bytes written";
 	return (char *)0;
 }
 
-char *
-jmscott_sendfile(int in, int out, off_t offset, off_t len)
+static char *
+apple_sendfile(int in_fd, int out_fd, long long *send_size)
 {
-	int total = len;
+	off_t sz = 0;
+
 	int status;
 AGAIN:
-#if defined(__APPLE__)
-	status = sendfile(in, out, offset, &len, (struct sf_hdtr *)0, 0);
-#else
-	status = sendfile(out, in, &offset, len);
-	len = status;
-#endif
+	status = sendfile(
+			in_fd,
+			out_fd,
+			(off_t)0,
+			&sz,
+			(struct sf_hdtr *)0,
+			0
+	);
 	if (status < 0) {
+		if (errno == EINTR || errno == EAGAIN)
+			goto AGAIN;
 		if (errno == ENOTSOCK)
-			return send_not_socket(in, out, offset, len);
+			return copyio(in_fd, out_fd, send_size);
+		return strerror(errno);
+	}
+
+	//  macos insures all bytes sent in first call to sendfile();
+
+	if (send_size)
+		*send_size = (long long)sz;
+	return (char *)0;
+}
+
+/*
+ *  send whole file, using sendfile() when appropriate or falling
+ *  back to read/write copy.
+ *
+ *  on non-apple, if *send_size == 0, then set *send_size to stat size.
+ *  on apple, *send_size update to actualy bytes written (up to EOF).
+ */
+char *
+jmscott_send_file(int in_fd, int out_fd, long long *send_size)
+{
+#if defined(__APPLE__)
+	return apple_sendfile(in_fd, out_fd, send_size);
+#else
+	long long sz;
+
+	struct stat st;
+
+	if (jmscott_fstat(in_fd, &st))
+		return strerror(errno);
+	sz = (long long)st.st_size;
+
+	//  probably linux sendfile() semantics.
+
+	size_t len = sz, nw;
+AGAIN:
+	nw = sendfile(out_fd, in_fd, (off_t *)0, len);
+	if (nw < 0 {
 		if (errno == EINTR || errno == EAGAIN)
 			goto AGAIN;
 		return strerror(errno);
 	}
-	if (len == 0)
-		return "unexpected end of file";
-	total -= len;
-	if (total == 0)
-		return (char *)0;
-	offset += len;
-	len = total;
+	len -= nw;
+	if (len > 0)
+		goto AGAIN;
+	if (send_size)
+		*send_size = sz;
+	return (char *
 	goto AGAIN;
+#endif
 }
