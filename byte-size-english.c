6d20097879b1d56a2a8c50bf193c753bd062485e
John the Scott <jmscott@setspace.com>
2023-12-31 15:30:04 -0600

    changed to s/b suffix

--- byte-size-english.c
+++ byte-size-english.c
@@ -1,17 +1,35 @@
 /*
  *  Synopsis:
- *	Convert byte count to english.
+ *	Convert byte count to brief english K,M,G,T,P,E,Z,Y,R,Q in 1024 powers.
  */
 #include <ctype.h>
 #include <string.h>
 
-extern int	errno;
-
 #include "jmscott/libjmscott.h"
 
-#define ROUND(n, d) ((((n) < 0) ^ ((d) < 0)) ? (((n) - (d)/2)/(d)) : (((n) + (d)/2)/(d)))
-
 char *jmscott_progname = "byte-size-english";
+
+extern int	errno;
+
+static char english[] = {
+	'B',			//  bytes
+	'K',			//  kilo
+	'M',			//  mega
+	'G',			//  giga
+	'T',			//  tera
+	'P',			//  peta
+	'E',			//  exa
+	'Z',			//  zetta
+	'Y',			//  yotta
+	'R',			//  ronna
+	'Q'			//  quetta
+};
+
+#define ROUND(n, d) (							\
+		(((n) < 0) ^ ((d) < 0))					\
+			?						\
+			(((n) - (d)/2)/(d)) : (((n) + (d)/2)/(d))	\
+		)
 
 static char *usage = "usage: byte-size-english <byte size>";
 
@@ -27,24 +45,6 @@ die2(char *msg1, char *msg2)
         jmscott_die2(1, msg1, msg2);
 }
 
-static void
-_write(char *unit, int ull)
-{
-        char buf[30];
-
-        char *p = jmscott_ulltoa(ull, buf);
-
-        *p++ = unit[0];
-	if (unit[1])
-        	*p++ = unit[1];
-        *p++ = '\n';
-	*p = 0;
-
-        if (jmscott_write(1, buf, p - buf) < 0)
-                die2("write() failed", strerror(errno));
-        _exit(0);
-}
-
 int
 main(int argc, char **argv)
 {
@@ -54,34 +54,31 @@ main(int argc, char **argv)
         if (*argv[1] == 0)
                 die("empty byte size");
 
+	//  extract byte size
+
 	unsigned long long bs;
 	char *err = jmscott_a2ui63(argv[1], &bs);
 	if (err)
 		die2("can not parse byte size", err);
 
 	unsigned long long r = 1024;
-        if (bs < r)
-                _write("B", bs);
+	for (int i = 0;  i < 11;  i++, r *= 1024) {
+		if (bs >= r)
+			continue;
 
-	r *= 1024;
-        if (bs < r)
-                _write("KB", ROUND(bs, r / 1024));
+        	char buf[30];
 
-	r *= 1024;
-        if (bs < r)
-		_write("MB", ROUND(bs, r / 1024));
-
-	r *= 1024;
-        if (bs < r)
-		_write("GB", ROUND(bs, r / 1024));
-
-	r *= 1024;
-        if (bs < r)
-		_write("TB", ROUND(bs, r / 1024));
-
-	r *= 1024;
-        if (bs < r)
-		_write("PB", ROUND(bs, r / 1024));
-        return 0;
+        	char *p = jmscott_ulltoa(ROUND(bs, r / 1024), buf);
+		*p++ = english[i];
+		if (i == 0)
+			*p++ = 's';
+		else
+			*p++ = 'b';
+		*p = '\n';
+		if (jmscott_write(1, buf, p - buf) < 0)
+			die2("write(1) failed", strerror(errno));
+		_exit(0);
+	}
+	die2("byte size too big", argv[1]);
 }
 
